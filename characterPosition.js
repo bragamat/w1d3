c54fe7c8350fb0d4c148324e191873763218a3dd
bragamat <mateuscbraga@gmail.com>
2018-03-14 16:43:01 +0000

    adjusting exer2

--- characterPosition.js
+++ characterPosition.js
@@ -3,14 +3,10 @@ function  countLetters(){
 var splitedSentence = sentence.split(' ').join('').split(''); 
   var sentenceAsObject = {}; 
     for (var i = 0; i < splitedSentence.length; i++){ 
-    var letter = splitedSentence[i]; 
-     if (sentenceAsObject[letter] === undefined){
-      sentenceAsObject[letter] = [i]; // i 
-    } 
-    else {
-      sentenceAsObject[letter].push(i);//+= ", " + i; 
-    }
-   }
-  return sentenceAsObject;
+    var letter = splitedSentence[i];
+sentenceAsObject[letter] === undefined ? sentenceAsObject[letter]=[i] :
+sentenceAsObject[letter].push(i);
+  }return sentenceAsObject;
 }
+
 console.log(countLetters(sentence));
